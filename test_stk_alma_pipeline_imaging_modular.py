20f9092e56997e0860b5f82a135ad859d992b23a
Joshua Hoskins <jhoskins@nrao.edu>
2021-11-09 16:25:29 -0500

    Slight reorganization of the repository. Remove older files from previous prototypes.

--- test_stk_alma_pipeline_imaging_modular.py
+++ test_stk_alma_pipeline_imaging_modular.py
@@ -113,6 +113,7 @@ try:
     from casatasks.private.imagerhelpers.parallel_imager_helper import PyParallelImagerHelper
     from casaviewer import imview
     
+    #from stk_utils import plot_utils as plt_utils
     import stk_utils.plot_utils as plt_utils
 
     CASA6 = True
